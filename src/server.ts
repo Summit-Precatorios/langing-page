047b3053274c0b68edd19b8aa2dee2d71c26782f
rayoneitalo <rayoneitalo@gmail.com>
2024-03-09 05:48:33 +0000

    feat: add assets folder

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,10 @@ import path from 'path';
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'))
+    res.sendFile(path.join(__dirname, '/index.html'))
 })
 
 app.listen(4001, () => console.log('Server is running at 4001'));
